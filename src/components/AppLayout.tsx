013e4a05bcc3e42d0bc98557c1de63615bfb310b
Abdullah Al Jubair Prince <jubairprince.cse@gmail.com>
2026-03-11 19:10:16 +0600

    oklkj

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -3,8 +3,8 @@ import { BottomNav } from "./BottomNav";
 
 export function AppLayout() {
     return (
-        <div className="relative min-h-screen w-full flex flex-col bg-background text-foreground overflow-hidden">
-            <main className="flex-1 overflow-y-auto pb-[72px] md:pb-[80px]">
+        <div className="relative flex flex-col bg-background text-foreground overflow-hidden" style={{ height: '100dvh' }}>
+            <main className="flex-1 overflow-y-auto pb-[72px] md:pb-[80px]" style={{ WebkitOverflowScrolling: 'touch' }}>
                 <Outlet />
             </main>
             <BottomNav />
